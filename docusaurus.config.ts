fe3f70603dfba801d9fafab3f07f53f21bdaf213
Jean-Frederic Mainville <jfmainville@outlook.com>
2026-06-18 17:43:56 +0000

    chore: remove tutorial section from sidebar and navbar

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -68,7 +68,7 @@ const config: Config = {
           type: "docSidebar",
           sidebarId: "tutorialSidebar",
           position: "left",
-          label: "Tutorial",
+          label: "Docs",
         },
         {
           href: "https://github.com/facebook/docusaurus",
@@ -84,8 +84,8 @@ const config: Config = {
           title: "Docs",
           items: [
             {
-              label: "Tutorial",
-              to: "/docs/intro",
+              label: "Terraform",
+              to: "/docs/terraform",
             },
           ],
         },
