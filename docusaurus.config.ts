270c0214367b908d5e9166486d9c68f0f8480117
Jean-Frederic Mainville <jfmainville@outlook.com>
2026-06-19 21:00:24 +0000

    chore: clean up docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -7,27 +7,18 @@ const config: Config = {
   tagline: "Opiniated Infrastructure-as-Code playbook.",
   favicon: "img/favicon.ico",
 
-  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
-    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+    v4: true,
   },
 
-  // Set the production url of your site here
   url: "https://iacplaybook.com",
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "facebook",
+  projectName: "docusaurus",
 
   onBrokenLinks: "throw",
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: "en",
     locales: ["en"],
@@ -49,7 +40,6 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
     image: "img/docusaurus-social-card.jpg",
     colorMode: {
       respectPrefersColorScheme: true,
@@ -90,7 +80,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} Nuagir inc.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Nuagir`,
     },
     prism: {
       theme: prismThemes.github,
