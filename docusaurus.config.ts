7b73211f6754098732d2d488224dcb0ddbed0768
Jean-Frederic Mainville <jfmainville@outlook.com>
2026-06-21 15:03:42 +0000

    chore: disable sidebar collapsing

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -27,6 +27,7 @@ const config: Config = {
           sidebarPath: "./sidebars.ts",
           routeBasePath: "/",
           editUrl: "https://github.com/nuagir/iac-playbook/tree/main/",
+          sidebarCollapsible: false,
         },
         theme: {
           customCss: "./src/css/custom.css",
